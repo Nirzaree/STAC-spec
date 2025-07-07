fa647c97ec6550941a2fd13ecdd0d5c196ced515
Vishnu S <vishnuvaishnav4059@gmail.com>
2025-07-07 16:07:24 +0530

    Updated script with the bug fix

--- constants.py
+++ constants.py
@@ -5,4 +5,14 @@ DEFAULT_START_DATE = datetime(2017, 7, 1)
 DEFAULT_END_DATE = datetime(2024, 6, 30)
 
 
-
+base_url="https://raw.githubusercontent.com/vishnus4059/STAC_common/master/data/Corestack%20Catalogs/gobindpur"
+data_url="https://raw.githubusercontent.com/vishnus4059/STAC_common/master/data/"
+raster_lulc_id="lulc_raster"
+raster_lulc_description="Land Use Land Cover raster map"
+raster_lulc_title="Raster layer"
+swb_vector_id="SWB vector"
+swb_vector_title="SWB Vector"
+swb_vector_description="SWB vector layer"
+id_main="gobindpur"
+title_main="GobindpurCatalog"
+description_main="Gobindpur catalog with LULC raster and SWB vector layers"
