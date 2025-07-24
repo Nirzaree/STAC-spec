78ca0e6cf360192e8b60b4b5c9b732505a5caf41
Vishnu S <vishnuvaishnav4059@gmail.com>
2025-07-24 13:42:50 +0530

    Updated multiple location catalog and classification ext

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ DEFAULT_START_DATE = datetime(2017, 7, 1)
 DEFAULT_END_DATE = datetime(2024, 6, 30)
 
 
-base_url="https://raw.githubusercontent.com/vishnus4059/STAC_common/master/"
-data_url="https://raw.githubusercontent.com/vishnus4059/STAC_common/master/data/"
+base_url="https://raw.githubusercontent.com/Nirzaree/STAC-spec/stac-spec-common/"
+data_url="https://raw.githubusercontent.com/Nirzaree/STAC-spec/stac-spec-common/data/"
 raster_lulc_id="lulc_raster"
 raster_lulc_description="Land Use Land Cover raster map"
 raster_lulc_title="Raster layer"
