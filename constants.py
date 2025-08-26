0cd36188acec2f4a6dc94dc70455523939b5e955
Nirzaree <nirzaree.svnit@gmail.com>
2025-08-26 16:32:53 +0530

    updating start and end date for terrain, and adding list of nodata class names

--- constants.py
+++ constants.py
@@ -4,6 +4,12 @@ from datetime import datetime
 DEFAULT_START_DATE = datetime(2017, 7, 1)
 DEFAULT_END_DATE = datetime(2024, 6, 30)
 
+SRTM_DEM_START_DATE = datetime(2020,2,11) #used for terrain raster as it 
+#uses SRTMGL1_003 product 
+# https://www.earthdata.nasa.gov/data/catalog/lpcloud-srtmgl1-003
+SRTM_DEM_END_DATE = datetime(2020,2,21)
+
+no_data_classnames_list = ['clear','background']
 
 base_url="https://raw.githubusercontent.com/Nirzaree/STAC-spec/stac-spec-common/"
 data_url="https://raw.githubusercontent.com/Nirzaree/STAC-spec/stac-spec-common/data/"
