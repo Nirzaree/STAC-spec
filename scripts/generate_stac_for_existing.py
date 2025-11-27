15518cc11adfe70a98fe2e1f0b8dd5a0cdbd0e93
Nirzaree <nirzaree.svnit@gmail.com>
2025-11-27 18:29:29 +0530

    tehsil name formatting

--- scripts/generate_stac_for_existing.py
+++ scripts/generate_stac_for_existing.py
@@ -134,7 +134,7 @@ def generate_stac_spec():
     print(active_tehsils)
 
     for tehsil in active_tehsils:
-        if (tehsil.lower() == 'masalia'):
+        if (tehsil.tehsil_name == 'Masalia'):
             state = tehsil.district.state
             district = tehsil.district
             print(state.state_name, district.district_name, tehsil.tehsil_name)
